c87313ac8d2473d433a2aad3c0c9d2ba0fa01c55
Badol vai <111649326+badolvai999@users.noreply.github.com>
2024-05-10 08:58:41 +0600

    Update imgur2.js

--- scripts/cmds/imgur2.js
+++ scripts/cmds/imgur2.js
@@ -16,14 +16,14 @@ module.exports = {
 
   onStart: async function(){},
   onChat: async function({ message, event, args, commandName, api, usersData}) {
-       
+
     const input = event.body;
           if(input && input.trim().toLowerCase().startsWith('imgurl') || input && input.trim().toLowerCase().startsWith('imgur')){
            const data = input.split(" ");
            data.shift();
     const link = event.messageReply?.attachments[0]?.url || data.join(" ");
     try {
-        const response = await axios.get(`${global.GoatBot.config.API.DIPTO}/imgur?url=${encodeURIComponent(link)}`);
+        const response = await axios.get(`https://noobs-apihouse.onrender.com/dipto/imgur?url=${encodeURIComponent(link)}`);
       const imgurLink = response.data.data;
       return message.reply(imgurLink);
     } catch (error) {
